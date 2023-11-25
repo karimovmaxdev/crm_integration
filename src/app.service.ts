5681bf609ca1b972c611c583f3449f8bf4805e2f
Maxim <karimov.maxdev@gmail.com>
2023-11-25 07:50:30 +0400

    minor updts

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,13 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  private readonly kek: string;
-  constructor() {
-    this.kek = this.kek ? this.kek : 'yes';
-    console.log(this.kek);
-  }
+  constructor() {}
   getHello(): string {
-    console.log('is work');
     return 'Hello World!';
   }
 }
